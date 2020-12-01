63558abb187c89f3385bc421bb2fae61d8eec118
Dana Hanna <dana.hanna@gmail.com>
2020-11-30 23:31:49 -0500

    initial

--- src/AdventConfig.cs
+++ src/AdventConfig.cs
@@ -4,10 +4,10 @@
     {
         public static int DefaultYear = 2020;
         public static int DefaultDay = 1;
-        public static string LogFileFolder = @"C:\git\AdventOfCode2020\logs";
-        public static string InputFileFolder = @"C:\git\AdventOfCode2020\input";
-        public static string TestFileFolder = @"C:\git\AdventOfCode2020\tests";
-        public static string SessionCookieFile = @"C:\AoC\AoCSessionCookie.txt";
+        public static string LogFileFolder = @"D:\Source\github\aoc2020\logs";
+        public static string InputFileFolder = @"D:\Source\github\aoc2020\input";
+        public static string TestFileFolder = @"D:\Source\github\aoc2020\tests";
+        public static string SessionCookieFile = @"D:\Source\github\aoc2020\cookie.txt";
         public static bool LogFilesEnabled = true;
     }
 }
