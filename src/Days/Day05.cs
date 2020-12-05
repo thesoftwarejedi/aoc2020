6cbbe602ad6838513155d77fb47270b0329e7040
Dana Hanna <dana.hanna@gmail.com>
2020-12-05 00:19:09 -0500

    day 5

--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -9,12 +9,56 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var lines = input.Lines();
+            var seats = new List<int>();
+            foreach (var line in lines)
+            {
+                var rows = Enumerable.Range(0, 128).ToArray();
+                var cols = Enumerable.Range(0, 8).ToArray();
+                var i = line.Aggregate((rows, cols), ((int[] r, int[] c) i, char a) => a switch
+                {
+                    'F' => (i.r.Take(i.r.Length / 2).ToArray(), i.c),
+                    'B' => (i.r.Skip(i.r.Length / 2).ToArray(), i.c),
+                    'L' => (i.r, i.c.Take(i.c.Length / 2).ToArray()),
+                    'R' => (i.r, i.c.Skip(i.c.Length / 2).ToArray())
+                });
+                int row = i.Item1[0];
+                int col = i.Item2[0];
+                var seat = row * 8 + col;
+                seats.Add(seat);
+            }
+            var max = seats.Max();
+            return max.ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var lines = input.Lines();
+            var seats = new List<int>();
+            foreach (var line in lines)
+            {
+                var rows = Enumerable.Range(0, 128).ToArray();
+                var cols = Enumerable.Range(0, 8).ToArray();
+                var i = line.Aggregate((rows, cols), ((int[] r, int[] c) i, char a) => a switch
+                {
+                    'F' => (i.r.Take(i.r.Length / 2).ToArray(), i.c),
+                    'B' => (i.r.Skip(i.r.Length / 2).ToArray(), i.c),
+                    'L' => (i.r, i.c.Take(i.c.Length / 2).ToArray()),
+                    'R' => (i.r, i.c.Skip(i.c.Length / 2).ToArray())
+                });
+                int row = i.Item1[0];
+                int col = i.Item2[0];
+                var seat = row * 8 + col;
+                seats.Add(seat);
+            }
+            seats.Sort();
+            var ss = seats.ToArray();
+            for (int i = 0; i < ss.Length-1; i++)
+            {
+                if (ss[i] + 1 != ss[i + 1])
+                    return (ss[i] + 1).ToString();
+            }
+            throw new Exception("that sucks");
         }
     }
 }
