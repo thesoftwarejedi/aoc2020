bf0fdd0886d7990fd70f2c5d20e39c8cd8ba8fb9
Dana Hanna <dana.hanna@gmail.com>
2020-12-05 00:57:30 -0500

    wow, i missed that one

--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -9,26 +9,7 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            var lines = input.Lines();
-            var seats = new List<int>();
-            foreach (var line in lines)
-            {
-                var rows = Enumerable.Range(0, 128).ToArray();
-                var cols = Enumerable.Range(0, 8).ToArray();
-                var i = line.Aggregate((rows, cols), ((int[] r, int[] c) i, char a) => a switch
-                {
-                    'F' => (i.r.Take(i.r.Length / 2).ToArray(), i.c),
-                    'B' => (i.r.Skip(i.r.Length / 2).ToArray(), i.c),
-                    'L' => (i.r, i.c.Take(i.c.Length / 2).ToArray()),
-                    'R' => (i.r, i.c.Skip(i.c.Length / 2).ToArray())
-                });
-                int row = i.Item1[0];
-                int col = i.Item2[0];
-                var seat = row * 8 + col;
-                seats.Add(seat);
-            }
-            var max = seats.Max();
-            return max.ToString();
+            return input.Lines().Select(a=> Convert.ToInt32(a.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'), 2)).Max().ToString();
         }
 
         public override string PartTwo(string input)
