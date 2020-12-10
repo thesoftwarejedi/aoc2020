e7c938ebe4441f4d174408b9abf8c3412f2e0e6e
Dana Hanna <dana.hanna@gmail.com>
2020-12-09 23:39:47 -0500

    9

--- src/Days/Day09.cs
+++ src/Days/Day09.cs
@@ -9,13 +9,32 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            var l = input.Lines();
-            return "todo";
+            var l = input.Longs().ToArray();
+            var p = l.Select((a, i) => (a, i)).Where(a => a.i >= 25).Select(a => (a.a, l.Skip(a.i - 25).Take(25).ToArray().GetCombinations(2).ToArray()));
+            var a = p.Where(a => !a.Item2.Any(b => b.Sum() == a.a)).First();
+
+            return a.ToString();
         }
 
         public override string PartTwo(string input)
         {
-            return "todo";
+            long ans = 133015568L;
+
+            var l = input.Longs().ToArray();
+
+            Random r = new Random();
+            while (true)
+            {
+                var r1 = r.Next(l.Length - 1);
+                var r2 = r.Next(r1 + 1, l.Length);
+                var sum = l.Skip(r1).Take(r2 - r1 + 1).Sum();
+                if (sum == ans)
+                {
+                    Log($"{r1} to {r2} is {l[r1]} to {l[r2]} = {sum}");
+                    var range = l.Skip(r1).Take(r2 - r1).ToArray();
+                    return (range.Min() + range.Max()).ToString();
+                }
+            }
         }
     }
 }
