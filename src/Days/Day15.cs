f26a726d57cb961080d0f02fe375f5fbd6d51fd0
Dana Hanna <dana.hanna@gmail.com>
2020-12-15 00:51:11 -0500

    15

--- src/Days/Day15.cs
+++ src/Days/Day15.cs
@@ -9,12 +9,56 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            // 7, 14, 0, 17, 11, 1, 2
+            var l = input.Split(',').Select(a => long.Parse(a));
+            var hs = new HashSet<long>(l);
+            var ls = new LinkedList<long>(l.Reverse());
+            long t = ls.Count;
+            while (true)
+            {
+                t++;
+                var v = ls.First.Value;
+                var n = hs.Add(v);
+                if (n)
+                {
+                    v = 0;
+                } else
+                {
+                    ls.SelectWithIndex().Where(a => a.item == v).Take(2).Select(a => a.index).Deconstruct(out int first, out int second);
+                    v = second - first;
+                }
+                ls.AddFirst(v);
+                if (t == 2020)
+                    return v.ToString();
+            }
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            // 7, 14, 0, 17, 11, 1, 2
+            var l = input.Split(',').Select(a => long.Parse(a));
+            var hs = new Dictionary<long, long>(l.Reverse().Skip(1).Reverse().SelectWithIndex().Select(a => new KeyValuePair<long, long>(a.item, a.index + 1)));
+            var v = l.Last();
+            long t = l.Count();
+            while (true)
+            {
+                if (t == 30000000)
+                    return v.ToString();
+
+                var n = hs.TryGetValue(v, out var el);
+
+                //speak
+                hs.Remove(v);
+                hs.Add(v, t);
+
+                if (!n)
+                    v = 0;
+                else
+                    v = t - el;
+
+                //next
+                t++;
+            }
         }
     }
 }
